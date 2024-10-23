a9be0b42ef68cb6f3b1d3f06ac4b857d3795c779
Michael Lyon <mlyon360@gmail.com>
2024-10-22 19:46:43 -0600

    fix errors

--- src/App.js
+++ src/App.js
@@ -3,37 +3,44 @@ import "./Components/DesktopNav";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import RunningStory from "./Components/RunningStory";
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RestaurantMenu from "./Components/ReactProjects/ReactProjects1/RestaurantMenu";
 // import Items from './Data';
-import items from "/Users/Lucinda/react-profile2/src/Components/ReactProjects/ReactProjects1/Data.jsx";
-import Menu from './Components/ReactProjects/ReactProjects1/Menu';
+import items from "./Components/ReactProjects/ReactProjects1/Data";
+import Menu from "./Components/ReactProjects/ReactProjects1/Menu";
 //I have error from below, trying to add stuff to make my reactproject 2 show up
-import ReactProjects2 from  './Components/ReactProjects/ReactProjects2';
+import ReactProjects2 from "./ReactProjects2/index";
 // Import the new FoodOrderingApp component
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <Navbar />
-      <Routes>
+      <div className="App">
+        <Navbar />
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/running-story" element={<RunningStory />} />
-          <Route path="/ReactProjects/ReactProjects1" element={<RestaurantMenu items={items} />} />
-          <Route path="/ReactProjects2/" element={<ReactProjects2 />} /> {/* New route for food ordering app */}
+          <Route
+            path="/ReactProjects/ReactProjects1"
+            element={<RestaurantMenu items={items} />}
+          />
+          <Route
+            path="/ReactProjects/ReactProjects2/"
+            element={<ReactProjects2 />}
+          />{" "}
+          {/* New route for food ordering app */}
           {/* Add other routes as needed */}
         </Routes>
-      {/* <Home /> */}
-    </div>
-  </BrowserRouter>
+        {/* <Home /> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
 
-//The import statement above is different from other imports 
-//you have done so far because App.js lives outside 
+//The import statement above is different from other imports
+//you have done so far because App.js lives outside
 //the NavBar container you created for this navigation menu.
---
+++ src/ReactProjects2/index.js
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <p>this is my page</p>;
+};
+
+export default Page;
